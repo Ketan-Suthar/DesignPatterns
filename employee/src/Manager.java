d6672d4551e0f84777cd71096d9d2acf007e6d15
Ketan Suthar <ketan.suthar@saama.com>
2023-07-21 18:36:26 +0530

    Composite: Employee Directory example added

---
+++ employee/src/Manager.java
@@ -0,0 +1,25 @@
+public class Manager implements Employee {
+    private String name;
+    private String position;
+    private String empId;
+    private int salary;
+
+    public Manager(final String name, final String position, final String empId, final int salary) {
+        this.name = name;
+        this.position = position;
+        this.empId = empId;
+        this.salary = salary;
+    }
+
+    @Override
+    public void showEmployeeDetails(final int tabs) {
+        System.out.println();
+        for (int i=0;i<tabs;++i) System.out.print("\t");
+        System.out.println(name + "'s Details");
+        for (int i=0;i<tabs;++i) System.out.print("\t");
+        System.out.print("\tid: " + empId);
+        System.out.print(", salary: " + salary);
+        System.out.print(", position: " + position);
+        System.out.println();
+    }
+}
